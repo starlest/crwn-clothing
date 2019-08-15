c4350738a3ec4e644d47af11e1d37dcff6884ee0
Edwin Chia <starlest@gmail.com>
2019-08-16 02:17:21 +0700

    created user and cart sagas for sign in, sign up and sign in with google

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,35 @@
+import { takeLatest, call, put, all } from "redux-saga/effects";
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils";
+
+import { fetchCollectionSuccess, fetchCollectionFailure } from "./shop.actions";
+
+import ShopActionTypes from "./shop.types";
+
+function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionFailure(error.message));
+  }
+}
+
+function* fetchCollectionsStart() {
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
+}
